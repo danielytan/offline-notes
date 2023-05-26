8612f59c98e5f00f4b064749fee608072ba5e95d
Daniel <danielytan1@gmail.com>
2023-05-26 18:15:54 +0900

    configured the service worker in config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.output.publicPath = './';
+    }
+
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/sw.js',
+        destination: '/_next/static/sw.js',
+      },
+    ];
+  },
+};
