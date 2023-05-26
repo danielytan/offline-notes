f6cc066a05fd7a556b63f405a77d3af6829c42d5
Daniel <danielytan1@gmail.com>
2023-05-26 18:17:06 +0900

    updated _document to register serviceworker

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,33 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                if ('serviceWorker' in navigator) {
+                  window.addEventListener('load', () => {
+                    navigator.serviceWorker.register('/sw.js').then(registration => {
+                      console.log('ServiceWorker registration successful with scope:', registration.scope);
+                    }).catch(error => {
+                      console.log('ServiceWorker registration failed:', error);
+                    });
+                  });
+                }
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
