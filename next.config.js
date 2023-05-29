9c7995534ee6fb3ba930dde0c2383f01032cdc5e
Daniel <danielytan1@gmail.com>
2023-05-30 02:45:11 +0900

    introduces serviceworker

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
+};
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  sw: 'sw.js'
+});
+
+module.exports = withPWA(nextConfig);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
 import NoteList from '../components/NoteList';
+import { useEffect } from 'react';
 
-export default function Notes() {
+export default function Home() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+          .register('/sw.js')
+          .then((registration) => {
+            console.log('Service Worker registered:', registration);
+          })
+          .catch((error) => {
+            console.log('Service Worker registration failed:', error);
+          });
+      });
+    }
+  }, []);
+
   return (
     <div>
       <NoteList />
