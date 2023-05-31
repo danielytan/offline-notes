49e25e59b54efbbcb1cf217f85dce4d41d8527f6
Daniel <danielytan1@gmail.com>
2023-05-31 17:28:59 +0900

    updated connectToDabase to reuse same MongoClient instance

--- src/api/mongo.js
+++ src/api/mongo.js
@@ -3,10 +3,16 @@ import { MongoClient } from 'mongodb';
 const uri = process.env.MONGODB_URI;
 const dbName = 'offline-notes-db';
 
+let client = null;
+
 async function connectToDatabase() {
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  if (client && client.topology && client.topology.isConnected()) {
+    console.log('Using existing MongoDB connection');
+    return client.db(dbName);
+  }
 
   try {
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
     console.log('Connected to MongoDB');
     return client.db(dbName);
