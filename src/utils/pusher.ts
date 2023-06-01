9599c2c75e5004c34627b30ae3ffe6cf29a9ef6e
Daniel <danielytan1@gmail.com>
2023-06-02 02:36:58 +0900

    updated pusherClient and getPusherServer to be defined only respectively if the code is being run client or serverside

--- src/utils/pusher.ts
+++ src/utils/pusher.ts
@@ -1,17 +1,27 @@
-  // Server-side
 import PusherServer from 'pusher';
 import PusherClient from 'pusher-js';
 
-// Client-side
-export const pusherClient: PusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
-});
+let pusherServer: PusherServer | undefined;
 
-export function getPusherServer() {
-  return new PusherServer({
-    appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID!,
-    key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
-    secret: process.env.NEXT_PUBLIC_PUSHER_SECRET!,
+// Client-side
+let pusherClient: PusherClient | undefined;
+if (typeof window !== 'undefined') {
+  pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
     cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
   });
 }
+
+// Server-side
+export function getPusherServer() {
+  if (!pusherServer) {
+    pusherServer = new PusherServer({
+      appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID!,
+      key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
+      secret: process.env.NEXT_PUBLIC_PUSHER_SECRET!,
+      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+    });
+  }
+  return pusherServer;
+}
+
+export { pusherClient };
